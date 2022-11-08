83361b5589004273fdb02f4ecc88f095799888a0
robertocannella <73362871+robertocannella@users.noreply.github.com>
2022-11-08 15:49:08 -0500

    Add content to index.php

--- wp-content/themes/rc-woo/index.php
+++ wp-content/themes/rc-woo/index.php
@@ -5,10 +5,33 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>RC Woo</title>
+    <title><?php    ?></title>
 </head>
-<h2>Woo Template</h2>
-<body>
+
+<body <?php  body_class();?>>
+<div id="page" class="site">
+    <header>
+        <section class="search">Search</section>
+        <section class="top-bar">
+            <div class="brand">Logo</div>
+            <div class="second-column">
+                <div class="account">Account</div>
+                <div class="main-menu">Menu</div>
+            </div>
+        </section>
+    </header>
+    <div class="content-area">
+        <section class="slider">Slider</section>
+        <section class="popular-products">Popular Products</section>
+        <section class="new-arrivals">New Arrivals</section>
+        <section class="deal-of-the-week">Deal of the Week</section>
+        <section class="woo-blog">News</section>
+    </div>
+    <footer>
+        <section class="footer-widgets">Footer Widgets</section>
+        <section class="copyright">Copyright</section>
+    </footer>
+</div>
 
 </body>
 </html>
