ed010df7473f3faec4a101ee4349ed4f84bb05b3
robertocannella <73362871+robertocannella@users.noreply.github.com>
2022-11-08 16:29:51 -0500

    Disable caching inside functions.php This should be enabled in Production environments

--- wp-content/themes/rc-woo/functions.php
+++ wp-content/themes/rc-woo/functions.php
@@ -8,6 +8,10 @@
  */
 
 function rc_woo_scripts(){
-    wp_enqueue_style( 'rc-woo-style', get_stylesheet_uri(), array(), '1.0', 'all');
+    // TO ENABLE caching, uncomment this line
+    // wp_enqueue_style( 'rc-woo-style', get_stylesheet_uri(), [], '1.0', 'all');
+
+    // TO DISABLE caching, uncomment this line.
+    wp_enqueue_style( 'rc-woo-style', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all');
 }
 add_action('wp_enqueue_scripts', 'rc_woo_scripts');
