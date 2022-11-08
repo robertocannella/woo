c34a28b7d0c65e63eb46577e3988344e7506737a
robertocannella <73362871+robertocannella@users.noreply.github.com>
2022-11-08 16:54:31 -0500

    Enqueue bootstrap in functions.php

--- wp-content/themes/rc-woo/functions.php
+++ wp-content/themes/rc-woo/functions.php
@@ -8,6 +8,10 @@
  */
 
 function rc_woo_scripts(){
+
+    wp_enqueue_script('bootstrap-js',get_template_directory_uri() . '/inc/bootstrap.min.js',['jquery'],'5.2.2', true);
+    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', [], '5.2.2', 'all');
+
     // TO ENABLE caching, uncomment this line
     // wp_enqueue_style( 'rc-woo-style', get_stylesheet_uri(), [], '1.0', 'all');
 
